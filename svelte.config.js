d9e1cac2c74d26e7e4932ab2c50bc80490aec3d3
albushgango <pupovalb@students.zhaw.ch>
2026-05-07 00:08:57 +0200

    chore: Netlify-Adapter und Build-Konfiguration
    - @sveltejs/adapter-netlify installiert (laut Modul-Anforderung)
    - svelte.config.js umgestellt von adapter-auto auf adapter-netlify
    - netlify.toml mit Build-Command, Publish-Verzeichnis und Node-20-Pinning
    - Build erzeugt jetzt explizit Netlify-Functions in .netlify/

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,10 +7,12 @@ const config = {
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
 	},
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		// Netlify-Adapter (laut Modul-Anforderung). Ohne Edge-Functions, weil wir
+		// MongoDB-Connections brauchen, die mit Edge Runtime schwierig sind.
+		adapter: adapter({
+			edge: false,
+			split: false
+		})
 	}
 };
 
